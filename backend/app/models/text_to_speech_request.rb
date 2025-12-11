6280cf05fa34d00c10b872d9bae41705b5f6f0b9
Jerinkc <jerinkc22@gmail.com>
2025-12-11 15:39:07 +0530

    More request status and validations

--- backend/app/models/text_to_speech_request.rb
+++ backend/app/models/text_to_speech_request.rb
@@ -1,6 +1,7 @@
 class TextToSpeechRequest < ApplicationRecord
-  enum status: { pending: 0, complete: 1 }
+  enum status: { pending: 0, converting: 2, uploading: 3, complete: 1 }
 
   has_one_attached :audio
   validates :text, presence: true
+  validates :text, length: { minimum: 2, maximum: 100 }
 end
